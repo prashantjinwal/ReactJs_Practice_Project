a1d258f9caad9aed60983e1fc2659709ff8b3cba
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-24 15:42:45 +0530

    setName Properties

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function Player() {
+
+  const PlayerName = useRef()
+  const [EnteredName, setEnteredName] = useState(null)
+  // const [submitted, setsubmitted] = useState(false)
+
+  // --- function for handling change name ---
+  // function handleChange(event){
+  //   // setsubmitted(false)
+  //   setEnteredName(event.target.value)
+  // }
+
+  function handleSubmit(){
+    // setsubmitted(true)
+    setEnteredName(PlayerName.current.value)
+
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {EnteredName ?? "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={PlayerName} type="text"  />
+        <button onClick={handleSubmit} >Set Name</button>
       </p>
     </section>
   );
