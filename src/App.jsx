3d16cea7b92ecf692025756d7f5656bbc928be36
prashantjinwal <98511988+prashantjinwal@users.noreply.github.com>
2024-06-25 13:27:23 +0530

    TimerChallenges

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Player from './components/Player.jsx';
+import TimerChallenges from './components/TimerChallenges.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenges title="Easy" targetTime={1}/>
+        <TimerChallenges title="Not Easy" targetTime={5}/>
+        <TimerChallenges title="Getting Tough" targetTime={10}/>
+        <TimerChallenges title="Pros Only" targetTime={15}/>
+      </div>
+
     </>
   );
 }
--- src/components/TimerChallenges.jsx
+++ src/components/TimerChallenges.jsx
@@ -1,16 +1,35 @@
+import { useRef, useState } from "react";
+
 export default function ({title, targetTime}){
+
+    const [ExpiredTime, setExpiredTime] = useState(false)
+    const [TimerStart, setTimerStart] = useState(false)
+    const timer = useRef()
+
+    function handleTimer(){
+        timer.current = setTimeout(()=>{
+            setExpiredTime(true)
+        }, targetTime*1000)
+
+        setTimerStart(true)
+    }
+
+    function handleStop(){
+        clearTimeout(timer.current)
+    }
+
     return(
         <section className="challenge">
             <h2>{title}</h2>
-
+            {ExpiredTime && <p> YOU LOST !!</p>}
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? "s" : ""}
             </p>
             <p>
-                <button>Start challenge</button>
+                <button onClick={TimerStart ? handleStop : handleTimer} >{ TimerStart ? "Stop" : "Start"} challenge</button>
             </p>
-            <p className="">
-                Time is running.../ Time inactive
+            <p className={TimerStart ? "active" : undefined}>
+               {TimerStart ?  "Time is running..." :  "Time inactive"}
             </p>
 
         </section>
